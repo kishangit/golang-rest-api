6a09f880ac6a58be802859f12a9bbbf2aca7fd61
Kishan Dalwadi <dalwadikishan456@gmail.com>
2024-06-21 11:58:45 +0530

    Added POST request, REST client extension of VS code, also added api-test using that extension

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"example.com/rest-api/models"
 	"net/http"
 )
 
@@ -9,12 +10,28 @@ func main () {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents (context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "Hello, World!",
-	})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent (context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
